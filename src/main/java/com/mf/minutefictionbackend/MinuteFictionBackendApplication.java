02845e01be9dbba4e0f8e5a915967e3e0893875e
ingelos <mailvaninge@gmail.com>
2024-07-08 16:20:12 +0200

    initial setup

---
+++ src/main/java/com/mf/minutefictionbackend/MinuteFictionBackendApplication.java
@@ -0,0 +1,13 @@
+package com.mf.minutefictionbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinuteFictionBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MinuteFictionBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mf/minutefictionbackend/MinuteFictionBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mf.minutefictionbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinuteFictionBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
