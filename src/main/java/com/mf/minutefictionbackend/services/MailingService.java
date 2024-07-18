3e8527030034d40a1a83078803d4746b890c78a1
ingelos <mailvaninge@gmail.com>
2024-07-18 21:04:11 +0200

    send mail service and controller setup - check back later to add Java Mail Sender

--- src/main/java/com/mf/minutefictionbackend/services/MailingService.java
+++ src/main/java/com/mf/minutefictionbackend/services/MailingService.java
@@ -3,21 +3,29 @@ package com.mf.minutefictionbackend.services;
 import com.mf.minutefictionbackend.dtos.inputDtos.MailingInputDto;
 import com.mf.minutefictionbackend.dtos.mappers.MailingMapper;
 import com.mf.minutefictionbackend.dtos.outputDtos.MailingOutputDto;
+import com.mf.minutefictionbackend.exceptions.ResourceNotFoundException;
 import com.mf.minutefictionbackend.models.Mailing;
+import com.mf.minutefictionbackend.models.User;
 import com.mf.minutefictionbackend.repositories.MailingRepository;
+import com.mf.minutefictionbackend.repositories.UserRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 
 @Service
 public class MailingService {
 
     private final MailingRepository mailingRepository;
-;
+    private final UserRepository userRepository;
 
-    public MailingService(MailingRepository mailingRepository) {
+//    private JavaMailSender mailSender;
+
+
+    public MailingService(MailingRepository mailingRepository, UserRepository userRepository) {
         this.mailingRepository = mailingRepository;
+        this.userRepository = userRepository;
     }
 
 
@@ -31,6 +39,29 @@ public class MailingService {
         return MailingMapper.mailingFromModelListToOutputList(allMailings);
     }
 
+//    public void sendMailing(Long mailingId) {
+//        Optional<Mailing> optionalMailing = mailingRepository.findById(mailingId);
+//        if(optionalMailing.isPresent()) {
+//            Mailing mailing = optionalMailing.get();
+//            List<User> subscribers = userRepository.findBySubscribedToMailing();
+//
+//            if(!subscribers.isEmpty()) {
+//                for (User user : subscribers) {
+//                    sendEmail(user.getEmail(), mailing.getTitle(), mailing.getContent());
+//                }
+//            }
+//        } else {
+//            throw new ResourceNotFoundException("No mailing found.");
+//        }
+//    }
+
+//    private void sendEmail(String to, String subject, String body) {
+//        SimpleMailMessage message = new SimpleMailMessage();
+//        message.setTo(to);
+//        message.setSubject(subject);
+//        message.setText(body);
+//        emailSender.send(message);
+//    }
 
 
 }
--- src/main/java/com/mf/minutefictionbackend/services/UserService.java
+++ src/main/java/com/mf/minutefictionbackend/services/UserService.java
@@ -69,7 +69,6 @@ public class UserService {
         } else {
             throw new UsernameNotFoundException(username);
         }
-
     }
 
     // setAuthorities
