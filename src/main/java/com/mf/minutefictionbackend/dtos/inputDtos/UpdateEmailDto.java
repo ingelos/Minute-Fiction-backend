b789a7ac60a251213ee1815475111ed04d1c413d
ingelos <mailvaninge@gmail.com>
2024-10-30 12:48:01 +0100

    updates regarding feedback

--- src/main/java/com/mf/minutefictionbackend/dtos/inputDtos/UpdateEmailDto.java
+++ src/main/java/com/mf/minutefictionbackend/dtos/inputDtos/UpdateEmailDto.java
@@ -1,7 +1,6 @@
 package com.mf.minutefictionbackend.dtos.inputDtos;
 
 import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
 
@@ -9,18 +8,16 @@ import java.util.HashSet;
 import java.util.Set;
 
 @Data
-public class UpdateUserInputDto {
+public class UpdateEmailInputDto {
 
     @Size(min=8, message = "Password needs to be at least 8 characters long")
-    private String newPassword;
-    @Size(min=8, message = "Password needs to be at least 8 characters long")
-    private String confirmPassword;
+    private String currentPassword;
     @Email(message = "Invalid email")
     private String email;
-    @NotNull
-    private boolean subscribedToMailing;
+//    @NotNull
+//    private boolean subscribedToMailing;
 
-    private Set<AuthorityInputDto> authorities = new HashSet<>();
+//    private Set<AuthorityInputDto> authorities = new HashSet<>();
 
 
 }
